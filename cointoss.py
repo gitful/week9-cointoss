6bb6e8a77ee031ec46b12d6c2376b7d9ebeb6c60
egrepnix <usertalk.grep@gmail.com>
2014-04-11 00:31:47 +0000

    well... something?

--- cointoss.py
+++ cointoss.py
@@ -3,18 +3,26 @@
 import re
 
 def saveState(state):
-	print("supposed to save state")
+	if state == []:
+		print("You didn't even play. Sucker.")
 
 def readState():
-	print("supposed to read state")
+	f = open(".cointoss_save", "r")
+	lines = f.readlines()
+	f.close()
+	if lines == []:
+		return []
+	return [lines[0], lines[1]]
 
-def flipCoin(state):
-	print("flipping the coin, matey")
+def flipCoin():
+	i = input(
 
-def endGame(state):
+def endGame(state, score):
 	print("supposed to end the jame... ^.^")
 
+score = 0
 state = readState()
-while True:
-	if not flipCoin(state):
-		endGame(state)
+print("Coin guessing game. All time high score: ", state[0], " correct by \"", state[1], "\".", sep="")
+while flipCoin():
+	score += 1
+endGame(state, score)
