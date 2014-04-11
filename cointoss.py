2f1c8efe8f94385f2564a34833426d2b4d9e943c
egrepnix <usertalk.grep@gmail.com>
2014-04-11 01:00:00 +0000

    asonethuasnotehusantoehusnth

--- cointoss.py
+++ cointoss.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+from random import choice
 
 def saveState(state):
 	if state == []:
@@ -15,14 +16,24 @@ def readState():
 	return [lines[0], lines[1]]
 
 def flipCoin():
-	i = input(
+	i = input("Predict heads or tails: ")
+	c = choice([True, False])
+	if c:
+		print("It is heads. ", end="")
+	else:
+		print("It is tails. ", end="")
+
+	if (re.match(r"^[hH]", i) and c) or (re.match(r"^[tT]", i) and not c):
+		return True
+	else:
+		return False
 
 def endGame(state, score):
 	print("supposed to end the jame... ^.^")
 
 score = 0
 state = readState()
-print("Coin guessing game. All time high score: ", state[0], " correct by \"", state[1], "\".", sep="")
+print("Coin guessing game. All time high score: ", state[0], " correct by \"", state[1], "\".", sep="", end="\n\n")
 while flipCoin():
 	score += 1
 endGame(state, score)
